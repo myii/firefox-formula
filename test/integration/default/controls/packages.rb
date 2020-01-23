67ee249112c8ffcca7df8c241153103ef895e91b
Dafydd Jones <dafydd@techneg.it>
2020-01-23 04:26:49 +0000

    feat(package): allow configuration of `extra_install_flags`

--- test/integration/default/controls/packages.rb
+++ test/integration/default/controls/packages.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# Overide by OS
 package_name = 'Mozilla Firefox'
+desktop_shortcut = 'C:\\Users\\Public\\Desktop\\Firefox.lnk'
 
 control 'firefox-package-install' do
   title 'Mozilla Firefox should be installed'
@@ -9,5 +9,9 @@ control 'firefox-package-install' do
   describe package(package_name) do
     it { should be_installed }
     its('version') { should eq '68.0' }
+  end
+
+  describe file(desktop_shortcut) do
+    it { should_not exist }
   end
 end
